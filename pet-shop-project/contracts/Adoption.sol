5d3b19db6a19c80b18cb82ba2e9bf85d523743e3
SKhunteta <shreyans.khunteta@gmail.com>
2019-09-23 17:39:34 -0700

    Fixing syntax fuckup

--- pet-shop-project/contracts/Adoption.sol
+++ pet-shop-project/contracts/Adoption.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0
+pragma solidity ^0.5.0;
 //In solidity, the carat means the version indicated or higher
 //pragma means "additional information only the compiler cares about"
 
