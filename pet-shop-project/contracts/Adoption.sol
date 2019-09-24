17530a9ee6a0681539e36cf4b6ceecb18b2f3b50
SKhunteta <shreyans.khunteta@gmail.com>
2019-09-23 17:35:32 -0700

    Created Adoption.sol file in contracts directory

---
+++ pet-shop-project/contracts/Adoption.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0
+//In solidity, the carat means the version indicated or higher
+//pragma means "additional information only the compiler cares about"
+
+contract Adoption {
+
+}
