842c783a67376f384ce654033833351add7b51a1
SKhunteta <shreyans.khunteta@gmail.com>
2019-09-24 17:36:36 -0700

    Create react app

--- DrizzleReactApp/drizzle-react-practice/contracts/MyStringStore.sol
+++ DrizzleReactApp/drizzle-react-practice/contracts/MyStringStore.sol
@@ -1,10 +1,9 @@
 pragma solidity ^0.5.0;
 
 contract MyStringStore {
+  string public myString = "Hello World";
 
-string public myString = "Hello world";
-
-function set(string memory x) public {
+  function set(string memory x) public {
     myString = x;
-}
+  }
 }
